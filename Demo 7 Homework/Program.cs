00693787e173ded240933aef9255eef1233b3e05
Saamari <sampsa_kautto@hotmail.com>
2016-02-02 11:07:22 +0200

    s

---
+++ Demo 7 Homework/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Demo_7_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string line = null;
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"d:\T1");
+            
+            // kysytään käyttäjältä rivejä
+            do
+            {
+                Console.Write("Give A number");
+                line = Console.ReadLine();
+
+                outputFile.WriteLine(line);
+            } while (line.Length != 0);
+            //suljetaan tiedosto
+            outputFile.Close();
+
+            try {
+                string text = File.ReadAllText(@"d:\T1");
+                Console.WriteLine(text);
+            }catch (FileNotFoundException)
+            {
+                Console.WriteLine(" File not found!!");
+            }
+        }        
+
+
+
+
+        }
+    }
+
---
+++ Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    class Program
+    {
+        int k;
+        double d;
+        static void Main(string[] args)
+        {
+            string line = null;
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"d:\T2");
+
+            // kysytään käyttäjältä rivejä
+            do
+            {
+                Console.Write("Give A number");
+                line = Console.ReadLine();
+
+                
+
+
+
+                outputFile.WriteLine(line);
+            } while (line.Length != 0);
+            //suljetaan tiedosto
+            outputFile.Close();
+            
+
+
+        }
+    }
+}
